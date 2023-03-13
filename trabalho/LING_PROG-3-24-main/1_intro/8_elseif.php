d791e9cfd105ae9c55fd918939d019fd5ed000f0
Gustavo <125514295+Gustavoaguiarlima@users.noreply.github.com>
2023-03-13 18:20:56 -0300

    Add files via upload

---
+++ trabalho/LING_PROG-3-24-main/1_intro/8_elseif.php
@@ -0,0 +1,18 @@
+<?php
+
+  $nome = "keven e felipe peixeiro";
+  
+  if($nome == "keven e felipe peixeiro"){
+	  echo "keven e felipe peixeiro estão pescando";
+	 
+  }elseif($nome == "keven e felipe"){
+	  echo "keven e felipe estão juntos<hr>";
+  }elseif($nome == "keven e gordo"){
+	  echo "felipe é gay<hr>";
+  }else{
+  echo "ninguem pegou nada<hr>";
+  }
+
+ 
+?>
+  

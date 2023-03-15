f610f377dc7b1acf902a4e6203db09eed843f940
Gustavo <125514295+Gustavoaguiarlima@users.noreply.github.com>
2023-03-15 18:18:43 -0300

    Add files via upload

---
+++ tarefa feita/1_intro/8_elseif.php
@@ -0,0 +1,12 @@
+<?php
+ $nome = "Gustavo & Manuel gomes";
+ if ($nome == "Gustavo & Maneul gomes"){
+	 echo "O Gustavo está estudando sozinho";
+ }elseif($nome == "Gustavo & Manuel gomes"){
+	 echo "Gustavo & Manuel gomes Estão estudando juntos";
+	 }elseif ($nome == "senhor caneta"){
+		 echo "senhor caneta";
+	 }else{
+		 echo "senhor caneta!!!!";
+	 }
+?>

13efc65f3a44578d96f68dc96d6236057fbc67f1
Gustavo <125514295+Gustavoaguiarlima@users.noreply.github.com>
2023-03-15 18:14:20 -0300

    Add files via upload

---
+++ Tarefas/1_intro/6_if_else.php
@@ -0,0 +1,27 @@
+<?php
+  
+  $idade = 19;
+  
+  if($idade <= 18) {
+	  echo 'vocè não pode entrar aqui! <HR> ';
+  } else {
+	  echo 'Seja Bem - vindo <HR> ';
+  }
+  
+   $permissão = 19;
+  
+  if($permissão < 18) {
+	  echo 'vocè não pode entrar aqui <hr> !';
+  } else {
+	  echo 'Seja Bem - vindo <hr> ';
+  }
+  
+   $entrada = 15;
+  
+  if($entrada <= 20) {
+	  echo 'vocè não pode entrar aqui! <HR> ';
+  } else {
+	  echo 'Seja Bem - vindo <HR> ';
+  }
+  
+?>

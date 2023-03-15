40cffa2cc83e329f3d57b1cdd7fc0846818e4ffc
Gustavo <125514295+Gustavoaguiarlima@users.noreply.github.com>
2023-03-15 17:34:01 -0300

    Add files via upload

---
+++ pasta bagunçado/1_intro/8_elseif.php
@@ -0,0 +1,12 @@
+<?php
+ $nome = "Gustavo";
+ if ($nome == "Gustavo"){
+	 echo "O Gustavo está estudando sozinho";
+ }elseif($nome == "Gustavo & Manuel"){
+	 echo "Gustavo & Manuel gomes Estão estudando juntos";
+	 }elseif ($nome == "Gustavo"){
+		 echo "O Manuel gomes está estudando";
+	 }else{
+		 echo "Manuel gomes gomes perdeu sua caneta azul!!!!";
+	 }
+?>
